5c50010bfcbc1e29542e44445f391d7328c5366b
AndroidPhrophet <ketuimevbore@yahoo.com>
2021-03-11 15:12:55 -0800

    mockito unit testing

---
+++ app/src/main/java/com/flowz/exoplayerapplication/calulator/Calculator.kt
@@ -0,0 +1,16 @@
+package com.flowz.exoplayerapplication.calulator
+
+import com.flowz.exoplayerapplication.calulator.Operators
+
+class Calculator(private val operators: Operators) {
+
+    fun addTwoNumbers(a:Int, b:Int): Int = operators.add(a, b)
+
+    fun subtractTwoNumbers(a: Int, b: Int): Int = operators.subtract(a, b)
+
+    fun multiplyTwoNumbers(a: Int, b: Int): Int = operators.multiply(a, b)
+
+    fun divideTwoNumbers(a: Int, b: Int): Int = operators.divide(a, b)
+
+
+}
